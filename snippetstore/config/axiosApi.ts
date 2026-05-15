7d8f58f14c1c652d24ff45bd525731d481895431
aalampatil <aalam.workacc@gmail.com>
2026-05-15 23:14:31 +0530

    feat: update environment variables for API URLs and modify axios configuration

--- snippetstore/config/axiosApi.ts
+++ snippetstore/config/axiosApi.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
+console.log("env test:", process.env.EXPO_PUBLIC_LOCAL_API_URL);
 
 export const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL ?? "http://localhost:3000",
+  baseURL: __DEV__
+    ? process.env.EXPO_PUBLIC_LOCAL_API_URL
+    : process.env.EXPO_PUBLIC_PRODUCTION_API_URL,
 });
